ca87d7b81253b32c233bb3bea28297d3ca5cac50
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2020-05-07 12:53:25 +0300

    implement artifact upload

--- buildScripts/upload_artifacts.sh
+++ buildScripts/upload_artifacts.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 set -e
+
+function getBranchReleaseVersion {
+  echo "${TRAVIS_BRANCH}" | cut -d'/' -f2
+}
+
+echo "LOG - Starting Artefact Upload"
+
+artifact_bucket="stefan-georgescu-deployment-artifacts"
+repository_key="ci-cd-example"
+s3_address="s3://${artifact_bucket}/${repository_key}"
+
+version_number=$(getBranchReleaseVersion)
+echo "LOG - Uploading release with version number ${version_number}"
+
+aws s3 cp release ${s3_address}/${version_number} --recursive --quiet --profile stg

c585478c55bb10b48a0d0d17a64afb280e7dac36
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2020-05-07 19:01:40 +0300

    remove wrong paranthesis

--- src/lambda.py
+++ src/lambda.py
@@ -5,7 +5,7 @@ def handler(event, context):
   url = "http://api.quotable.io/random"
   response = requests.get(url)
   response_json = response.json()
-  return response_json["content"])
+  return response_json["content"]
 
 
 if __name__ == "__main__":

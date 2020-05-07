fd33288f5a3b96ef7877eaed0419aab9c6579cca
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2020-05-07 18:28:04 +0300

    add missing parameters

--- buildScripts/helpers/functions/deploy_lambda.sh
+++ buildScripts/helpers/functions/deploy_lambda.sh
@@ -2,7 +2,7 @@
 
 # $1 - Release files path compared to current working directory
 # $2 - name of lambda function in aws env
-# $3 - lambda base name
+# $3 - lambda file name
 # $3 - AWS profile to use with the aws cli
 # $4 - AWS region
 
@@ -11,7 +11,7 @@ function deployLambda {
   
   lambda_path=$1
   lambda_aws_name=$2
-  lambda_base_name=$3
+  lambda_file_name=$3
   aws_profile=$4
   aws_region=$5
 
@@ -22,14 +22,14 @@ function deployLambda {
 
   cd "${lambda_path}"
 
-  if [ -f "${lambda_base_name}-deps.zip" ]; then
+  if [ -f "${lambda_file_name}-deps.zip" ]; then
     echo "LOG - Found Lambda dependencies. Creating Lambda Layer."
 
-    layer_version_arn=$(aws lambda publish-layer-version --layer-name "dependencies-${lambda_aws_name}" --zip-file fileb://./${lambda_base_name}-deps.zip --compatible-runtimes python3.8 | jq -r .LayerVersionArn)
+    layer_version_arn=$(aws lambda publish-layer-version --layer-name "dependencies-${lambda_aws_name}" --zip-file fileb://./${lambda_file_name}-deps.zip --compatible-runtimes python3.8 | jq -r .LayerVersionArn)
     aws lambda update-function-configuration --function-name "${lambda_aws_name}" --layers ${layer_version_arn}
   fi
 
   echo "LOG - Updating function code."
-  aws lambda update-function-code --function-name "${lambda_aws_name}" --zip-file fileb://./${lambda_base_name}.zip
+  aws lambda update-function-code --function-name "${lambda_aws_name}" --zip-file fileb://./${lambda_file_name}.zip
 
 }
